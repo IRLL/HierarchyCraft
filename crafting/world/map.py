0df121b9471aa3eeaac2d7bf97aa1cab7a8115d2
MathisFederico <mathfederico@gmail.com>
2021-02-19 16:07:19 +0100

    Added Zone with docstring and tests

---
+++ crafting/world/map.py
@@ -0,0 +1,57 @@
+# Crafting a gym-environment to simultate inventory managment
+# Copyright (C) 2021 Mathïs FEDERICO <https://www.gnu.org/licenses/>
+
+""" Map and Zone
+
+Defining Zone and Map to give an abstract way of doing spacialization.
+The Zones have special properties for finding or crafting items.
+The Map give a graph joinning all Zones.
+
+"""
+
+from typing import List
+
+class Zone():
+
+    """ Zones are to represent abstract places in the world.
+
+    Zone have specific properties that can change how items can be gathered
+    and how craftings are done.
+
+    Attributes:
+        zone_id (int): Unique zone identification number.
+        name (str): Zone name.
+        properties (dict): Dictionary of properties.
+        items (dict): Dictionary associating an item_id
+            with the tools needed to gather it and how efficient it is.
+
+    """
+
+    def __init__(self, zone_id: int, name: str, properties: dict, items: dict):
+        """ Zones are to represent abstract places in the world.
+
+        Zone have specific properties that can change how items can be gathered
+        and how craftings are done.
+
+        Args:
+            zone_id: Unique zone identification number.
+            name: Zone name.
+            properties: List of all properties names.
+            items: Dictionary associating an item_id
+                with the tools needed to gather it and how efficient it is.
+        """
+        self.zone_id = zone_id
+        self.name = name
+        self.items = items
+        self.properties = properties
+
+    def __str__(self):
+        return f"{self.name.capitalize()}({self.zone_id})"
+
+    def __repr__(self):
+        return f"{self.name.capitalize()}({self.zone_id}){self.properties}{self.items}"
+
+class Map():
+
+    def __init__(self, zones: List[Zone], graph: dict):
+        pass
