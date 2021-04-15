5c67c1a547347bcdfa93e572ddb0e3566ea1627a
Mathïs Fédérico <mathfederico@gmail.com>
2021-04-15 17:25:53 +0200

    Added graph.py

--- crafting/world/world.py
+++ crafting/world/world.py
@@ -6,13 +6,10 @@
 """
 
 from typing import List
-from copy import deepcopy
 
 import numpy as np
 import networkx as nx
 
-import matplotlib.pyplot as plt
-plt.style.use('dark_background')
 import matplotlib.patches as mpatches
 from matplotlib.legend_handler import HandlerPatch
 
@@ -20,6 +17,7 @@ from crafting.world.zones import Zone
 from crafting.world.items import Item, Tool
 from crafting.world.recipes import Recipe
 from crafting.option import GetItem, GoToZone
+from crafting.graph import compute_color, compute_levels, leveled_layout
 
 
 class World():
@@ -285,11 +283,11 @@ class World():
                             type="drop", color=[0, 1, 0, 1]
                         )
 
-        compute_levels(graph)
+        compute_levels(graph, weak_edges_type=["tool_requirement"])
         return graph
 
     # pylint: disable=invalid-name
-    def draw_requirements_graph(self, ax:plt.Axes):
+    def draw_requirements_graph(self, ax):
         """ Draw the requirement graph on a given Axes.
 
         Args:
@@ -417,182 +415,3 @@ class World():
             world_str += "\n  " + str(recipe)
 
         return world_str
-
-
-def compute_color(graph:nx.DiGraph):
-    alphas = [1, 1, 0.9, 0.8, 0.7, 0.5, 0.4, 0.3]
-    for node in graph.nodes():
-        successors = list(graph.successors(node))
-        for succ in successors:
-            alpha = 0.2
-            if graph.nodes[node]['level'] < graph.nodes[succ]['level']:
-                if len(successors) < len(alphas):
-                    alpha = alphas[len(successors)-1]
-            else:
-                graph.edges[node, succ]['linestyle'] = "dashed"
-            if isinstance(graph.edges[node, succ]['color'], list):
-                graph.edges[node, succ]['color'][3] = alpha
-
-def compute_levels(graph:nx.DiGraph, weak_edges_type:List[str]=("tool_requirement",)):
-    """ Compute the hierachical levels of all DiGraph nodes given some weak_edges.
-
-    Adds the attribute 'level' for each node in the given graph.
-
-    Args:
-        graph: A networkx DiGraph.
-        weak_edges_type: List of weak edges types (only the min level of those will be considered).
-            Require edges to have the attribute 'type'.
-
-    """
-
-    def _compute_level_dependencies(graph, node, predecessors):
-        strong_dependencies = [0]
-        weak_dependencies = []
-        incomplete = False
-        for pred in predecessors:
-            try:
-                pred_level = graph.nodes[pred]['level']
-                if graph.edges[pred, node]['type'] in weak_edges_type:
-                    weak_dependencies.append(pred_level)
-                else:
-                    strong_dependencies.append(pred_level)
-            except KeyError:
-                if graph.edges[pred, node]['type'] not in weak_edges_type:
-                    incomplete = True
-        dependencies = strong_dependencies
-        if len(weak_dependencies) > 0:
-            dependencies += [min(weak_dependencies)]
-        return 1 + max(dependencies), incomplete
-
-    all_nodes_have_level = False
-    while not all_nodes_have_level:
-        all_nodes_have_level = True
-        for node in graph.nodes():
-            predecessors = list(graph.predecessors(node))
-
-            if len(predecessors) == 0:
-                level = 0
-            else:
-                level, incomplete = _compute_level_dependencies(graph, node, predecessors)
-                if incomplete:
-                    all_nodes_have_level = False
-            if 'level' in graph.nodes[node] and graph.nodes[node]['level'] != level:
-                all_nodes_have_level = False
-
-            graph.nodes[node]['level'] = level
-
-# pylint: disable=protected-access
-def leveled_layout(graph:nx.DiGraph, center=None):
-    """ Compute positions for a levelled DiGraph.
-
-    Requires each node to have a 'level' attribute.
-
-    Args:
-        graph: A networkx DiGraph.
-        center: Center of the graph layout.
-
-    """
-    graph, center = nx.drawing.layout._process_params(graph, center, dim=2)
-
-    nodes_by_level = {}
-    for node, node_data in graph.nodes(data=True):
-        level = node_data['level']
-        try:
-            nodes_by_level[level].append(node)
-        except KeyError:
-            nodes_by_level[level] = [node]
-
-    pos = {}
-    step_size = 1 / max(len(nodes_by_level[level]) for level in nodes_by_level)
-    spacing = np.arange(0, 1, step=step_size)
-    for level in nodes_by_level:
-        n_nodes_in_level = len(nodes_by_level[level])
-        if n_nodes_in_level > 1:
-            positions = np.linspace(0, len(spacing)-1, n_nodes_in_level,
-                endpoint=True, dtype=np.int32)
-            positions = spacing[positions]
-        else:
-            positions = [spacing[(len(spacing)-1)//2]]
-
-        for i, node in enumerate(nodes_by_level[level]):
-            pos[node] = [positions[i], -level]
-
-    def energy(pos, nodes_by_level, nodes_strenght=1, edges_strenght=2):
-
-        def dist(x, y):
-            x_arr, y_arr = np.array(x), np.array(y)
-            return np.linalg.norm(x_arr-y_arr)
-
-        energy = 0
-        for level in nodes_by_level:
-            for node in nodes_by_level[level]:  
-                energy += nodes_strenght * sum(
-                    np.square(dist(pos[node], pos[n]))
-                    for n in nodes_by_level[level] if n != node
-                )
-                energy -= sum(
-                    edges_strenght \
-                        # *graph.edges[pred, node]['color'][3]\
-                        /abs(graph.nodes[node]['level'] - graph.nodes[pred]['level'])\
-                        /max(1e-6, dist(pos[node], pos[pred]))
-                    for pred in graph.predecessors(node)
-                )
-                energy -= sum(
-                    edges_strenght \
-                        # *graph.edges[node, succ]['color'][3]\
-                        /abs(graph.nodes[node]['level'] - graph.nodes[succ]['level'])\
-                        /max(1e-6, dist(pos[node], pos[succ]))
-                    for succ in graph.successors(node)
-                )
-        return energy
-
-    def neighbor(pos, step_size):
-        pos_copy = deepcopy(pos)
-        choosen_node = np.random.choice(list(pos_copy.keys()))
-        try:
-            choosen_node = int(choosen_node)
-        except ValueError:
-            pass
-        x, y = pos_copy[choosen_node][0], pos_copy[choosen_node][1]
-
-        if x <= 0:
-            sign = 1
-        elif x >= 1:
-            sign = -1
-        else:
-            sign = np.random.choice((-1, 1))
-
-        new_pos = [x + sign * step_size, y]
-
-        for n in pos:
-            if n != choosen_node and np.all(np.isclose(new_pos, pos_copy[n])):
-                pos_copy[choosen_node], pos_copy[n] = pos_copy[n], pos_copy[choosen_node]
-                return pos_copy
-
-        pos_copy[choosen_node] = new_pos
-        return pos_copy
-
-    def prob_keep(temperature, delta_e, kb=1):
-        return min(1, np.exp(delta_e/kb/temperature))
-
-    max_iterations = 1000
-    max_iters_without_new = 100
-    iters_without_new = 0
-    boltzmann_factor = 50
-    initial_temperature = 0.1
-    energy_pos = energy(pos, nodes_by_level)
-    for k in range(max_iterations):
-        new_pos = neighbor(pos, step_size)
-        new_e = energy(new_pos, nodes_by_level)
-        temperature = initial_temperature/(k+1)
-        iters_without_new += 1
-        prob = prob_keep(temperature, energy_pos - new_e, boltzmann_factor)
-        if np.random.random() < prob:
-            print(f"{k}\t({prob:.0%})\t{energy_pos:.2f}->{new_e:.2f}", end='\r')
-            pos, energy_pos = new_pos, new_e
-            iters_without_new = 0
-
-        if iters_without_new >= max_iters_without_new:
-            break
-
-    return pos, nodes_by_level
