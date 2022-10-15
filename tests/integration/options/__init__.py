757486e53da8d104446c80e87aa528b72cdbe33b
Mathïs Fédérico <mathfederico@gmail.com>
2022-10-15 22:55:54 +0200

    :umbrella: :bug: :fire: Remove init files in tests

--- tests/integration/options/__init__.py
+++
@@ -1,4 +0,0 @@
-# Crafting a gym-environment to simultate inventory managment
-# Copyright (C) 2021-2022 Mathïs FEDERICO <https://www.gnu.org/licenses/>
-
-""" Module regrouping all crafting options tests """
