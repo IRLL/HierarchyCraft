f309a6e7b8718b9c45f725cfb69482dae6aa11d9
Mathïs Fédérico <mathfederico@gmail.com>
2022-10-15 23:10:19 +0200

    :hammer: Switch to pyproject.toml setup config

--- setup.py
+++ setup.py
@@ -1,52 +1,6 @@
 # Crafting a gym-environment to simultate inventory managment
 # Copyright (C) 2021-2022 Mathïs FEDERICO <https://www.gnu.org/licenses/>
 
-import pathlib
-from setuptools import setup, find_packages
+from setuptools import setup
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.rst").read_text()
-
-
-def get_version():
-    with open("VERSION", encoding="utf-8") as f:
-        version = f.read().strip()
-    return version
-
-
-VERSION = get_version()
-
-
-def get_requirements():
-    with open("requirements.txt", encoding="utf-8") as f:
-        requirements = f.readlines()
-    return requirements
-
-
-REQUIREMENTS = get_requirements()
-
-setup(
-    name="crafting",
-    version=VERSION,
-    author="Mathïs Fédérico",
-    author_email="mathfederico@gmail.com",
-    description="A gym-environment to simultate inventory managment",
-    long_description=README,
-    long_description_content_type="text/x-rst",
-    url="https://github.com/MathisFederico/Crafting",
-    packages=find_packages(exclude=("tests*", "docs*")),
-    include_package_data=True,
-    package_data={
-        "examples_ressources": ["crafting/examples/**/resources/*"],
-    },
-    install_requires=REQUIREMENTS,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-)
+setup()
