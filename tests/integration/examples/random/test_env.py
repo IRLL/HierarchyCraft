9c9681ea9ef6d9feb7d2b21b018e5ad2c1fe08e0
Mathïs Fédérico <mathfederico@gmail.com>
2022-11-28 03:13:45 +0100

    :wrench: Fix test naming

--- tests/integration/examples/random/test_env.py
+++ tests/integration/examples/random/test_env.py
@@ -9,20 +9,20 @@ from networkx import is_isomorphic
 from crafting.examples.random.env import RandomCraftingEnv
 
 
-class TestTasks:
+class TestRandomCrafting:
 
-    """Tasks of the MineCrafting environment"""
+    """Test the RandomCrafting environment"""
 
     @pytest.fixture(autouse=True)
     def setup(self):
         """Setup test fixtures."""
         n_items = 10
         self.env_settings = {
-            'n_items': n_items,
-            'n_tools': int(0.1*n_items),
-            'n_findables': int(0.3*n_items),
-            'n_required_tools': [0.25, 0.4, 0.2, 0.1, 0.05],
-            'n_inputs_per_craft': [0.1, 0.6, 0.3],
+            "n_items": n_items,
+            "n_tools": int(0.1 * n_items),
+            "n_findables": int(0.3 * n_items),
+            "n_required_tools": [0.25, 0.4, 0.2, 0.1, 0.05],
+            "n_inputs_per_craft": [0.1, 0.6, 0.3],
         }
 
     def test_same_seed_same_requirements_graph(self):
