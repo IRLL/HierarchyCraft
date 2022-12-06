2a4ad9b035c8b405533cfb7bcea785edf2e1b72c
Mathïs Fédérico <mathfederico@gmail.com>
2022-12-06 16:19:10 +0100

    :umbrella: Add some TowerCrafting tests

--- tests/integration/examples/simple/test_tower.py
+++ tests/integration/examples/simple/test_tower.py
@@ -9,27 +9,106 @@ from hypothesis.strategies import integers
 from networkx import is_isomorphic, DiGraph
 
 from crafting.examples.simple import TowerCraftingEnv
+from option_graph.option import Option
 
 
-@given(integers(1, 3), integers(1, 3))
-def test_tower_requirements_graph(height: int, width: int):
+def test_tower_requirements_graph_1_1():
+    """should have expected structure
+
+    #1#
+    #0#
+
+    """
+    height = 1
+    width = 1
     env = TowerCraftingEnv(height=height, width=width)
     expected_graph = DiGraph()
-    for layer in range(1, height):
-        layer_id = layer * width
-        prev_layer_id = (layer - 1) * width
-        for item_layer_id in range(width):
-            for required_id in range(width):
-                expected_graph.add_edge(
-                    prev_layer_id + required_id,
-                    layer_id + item_layer_id,
-                )
-
-    last_layer_id = (height - 1) * width
-    last_item_id = height * width + 1
-    for required_id in range(width):
-        expected_graph.add_edge(last_layer_id + required_id, last_item_id)
+    expected_graph.add_edge(0, 1)
 
     for recipe in env.world.recipes:
         print(recipe)
     check.is_true(is_isomorphic(env.world.get_requirements_graph(), expected_graph))
+
+
+def test_tower_requirements_graph_1_2():
+    """should have expected structure
+
+    #2#
+    #01#
+
+    """
+    height = 1
+    width = 2
+    env = TowerCraftingEnv(height=height, width=width)
+    expected_graph = DiGraph()
+    expected_graph.add_edge(0, 2)
+    expected_graph.add_edge(1, 2)
+
+    for recipe in env.world.recipes:
+        print(recipe)
+    check.is_true(is_isomorphic(env.world.get_requirements_graph(), expected_graph))
+
+
+def test_tower_requirements_graph_3_2():
+    """should have expected structure
+
+    #6##
+    #45#
+    #23#
+    #01#
+
+    """
+    height = 3
+    width = 2
+    env = TowerCraftingEnv(height=height, width=width)
+    expected_graph = DiGraph()
+    expected_graph.add_edge(5, 6)
+    expected_graph.add_edge(4, 6)
+    expected_graph.add_edge(2, 5)
+    expected_graph.add_edge(3, 5)
+    expected_graph.add_edge(2, 4)
+    expected_graph.add_edge(3, 4)
+    expected_graph.add_edge(0, 3)
+    expected_graph.add_edge(1, 3)
+    expected_graph.add_edge(0, 2)
+    expected_graph.add_edge(1, 2)
+
+    for recipe in env.world.recipes:
+        print(recipe)
+    check.is_true(is_isomorphic(env.world.get_requirements_graph(), expected_graph))
+
+
+def run_solve(env: TowerCraftingEnv, solver: Option) -> int:
+    """Count how much steps are needed for the solver to finish.
+
+    Args:
+        env (CraftingEnv): The Crafting environment containing an finishing task.
+        solver (Option): The solver to test the lenght of.
+
+    Returns:
+        int: Number of steps needed for the solver to complete the task.
+    """
+    step = 0
+    done = False
+    observation = env.reset()
+    while not done:
+        action = solver(observation)
+        observation, _, done, _ = env.step(action)
+        step += 1
+
+    assert step < env.max_step
+    return step
+
+
+@given(integers(1, 3), integers(1, 3))
+def test_tower_can_solve(height: int, width: int):
+    env = TowerCraftingEnv(height=height, width=width, use_old_gym_format=True)
+    all_options = env.world.get_all_options()
+    solving_option: Option = all_options[f"Get {env.tasks[0].goal_item}"]
+    n_steps = run_solve(env, solver=solving_option)
+
+
+def test_tower_accessible_items():
+    env = TowerCraftingEnv(height=2, width=3)
+    for foundable_item in env.world.items[: env.width]:
+        check.is_in(foundable_item, env.world.zones[0].items)
