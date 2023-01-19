3d2764afb44b2c71acc726f066c29fb582442ed3
Mathïs Fédérico <mathfederico@gmail.com>
2023-01-19 15:53:22 +0100

    :tada: Add gym.make for CraftingEnchantingTable

--- src/crafting/__init__.py
+++ src/crafting/__init__.py
@@ -7,3 +7,15 @@ from crafting.env import CraftingEnv
 from crafting.examples.minecraft.env import MineCraftingEnv
 from crafting.examples.random.env import RandomCraftingEnv
 from crafting.task import Task
+
+try:
+    import gym
+
+    gym.register(
+        id="CraftingEnchantingTable-v1",
+        entry_point="crafting.examples.minecraft.env:MineCraftingEnv",
+        max_episode_steps=200,
+        kwargs={"observe_legal_actions": False},
+    )
+except ImportError:
+    pass
