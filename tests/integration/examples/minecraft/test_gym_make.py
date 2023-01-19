0558ed3469970d617710de58282568880de94e6a
Mathïs Fédérico <mathfederico@gmail.com>
2023-01-19 16:44:12 +0100

    🧪 Test gym.make for obtaining enchanting table in MineCrafting

--- tests/integration/examples/minecraft/test_gym_make.py
+++ tests/integration/examples/minecraft/test_gym_make.py
@@ -6,9 +6,19 @@ import gym
 from crafting.examples.minecraft.env import MineCraftingEnv
 
 
-def test_gym_make_minecrafting():
+def test_no_reward_gym_make():
     env: MineCraftingEnv = gym.make("MineCrafting-NoReward-v1")
     check.equal(env.fail_penalty, 1)
     check.equal(env.moving_penalty, 0)
     check.equal(env.timestep_penalty, 0)
     check.equal(len(env.tasks), 0)
+
+
+def test_enchanting_table_gym_make():
+    env: MineCraftingEnv = gym.make("MineCrafting-EnchantingTable-v1")
+    check.equal(env.fail_penalty, 1)
+    check.equal(env.moving_penalty, 0.4)
+    check.equal(env.timestep_penalty, 0.1)
+    check.equal(len(env.tasks), 1)
+    task = env.tasks[0]
+    check.equal(task.name, "obtain_enchanting_table")
