1eb8785ed4142f9ec4da78ff7fdfb57e3ae886c9
Mathïs Fédérico <mathfederico@gmail.com>
2023-01-19 16:03:24 +0100

    :tada: Add gym make for RandomCrafting-n10

--- src/crafting/__init__.py
+++ src/crafting/__init__.py
@@ -17,5 +17,18 @@ try:
         max_episode_steps=200,
         kwargs={"observe_legal_actions": False},
     )
+
+    gym.register(
+        id="RandomCrafting-n10-v1",
+        entry_point="crafting.examples.random.env:RandomCraftingEnv",
+        max_episode_steps=200,
+        kwargs={
+            "n_items": 10,
+            "n_tools": 0,
+            "n_findables": 1,
+            "n_required_tools": [1.0],
+            "n_inputs_per_craft": [0.1, 0.6, 0.3],
+        },
+    )
 except ImportError:
     pass
