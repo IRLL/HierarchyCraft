6a9929349c9f5d6a24a7001f326f8a91bd36e54a
Mathïs Fédérico <mathfederico@gmail.com>
2023-01-19 16:36:03 +0100

    🧪 Test gym.make for MineCrafting-NoReward

--- tests/integration/examples/minecraft/test_gym_make.py
+++ tests/integration/examples/minecraft/test_gym_make.py
@@ -1,7 +1,14 @@
+import pytest_check as check
+
+
 import gym
 
 from crafting.examples.minecraft.env import MineCraftingEnv
 
 
 def test_gym_make_minecrafting():
-    env = gym.make("CraftingEnchantingTable-v1")
+    env: MineCraftingEnv = gym.make("MineCrafting-NoReward-v1")
+    check.equal(env.fail_penalty, 1)
+    check.equal(env.moving_penalty, 0)
+    check.equal(env.timestep_penalty, 0)
+    check.equal(len(env.tasks), 0)
