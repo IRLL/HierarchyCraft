ab2a9a0b0f0f1f223277fb2568f509b7597faba4
Mathïs Fédérico <mathfederico@gmail.com>
2023-01-21 18:34:36 +0100

    :umbrella: :wrench: Fix BEDROCK require stone->iron

--- tests/unit/examples/minecraft/test_requirements_graph.py
+++ tests/unit/examples/minecraft/test_requirements_graph.py
@@ -1,7 +1,7 @@
 import pytest_check as check
 
 from crafting.examples.minecraft.world import McWorld
-from crafting.examples.minecraft.items import WOOD, STONE_PICKAXE
+from crafting.examples.minecraft.items import WOOD, IRON_PICKAXE
 from crafting.examples.minecraft.zones import FOREST, BEDROCK, UNDERGROUND
 
 
@@ -13,12 +13,12 @@ def test_wood_require_forest():
     check.is_true(graph.has_edge(FOREST, WOOD))
 
 
-def test_bedrock_require_stone_pickaxe():
+def test_bedrock_require_iron_pickaxe():
     mc_world = McWorld()
     graph = mc_world.requirements_graph
     check.is_true(graph.has_node(BEDROCK))
-    check.is_true(graph.has_node(STONE_PICKAXE))
-    check.is_true(graph.has_edge(STONE_PICKAXE, BEDROCK))
+    check.is_true(graph.has_node(IRON_PICKAXE))
+    check.is_true(graph.has_edge(IRON_PICKAXE, BEDROCK))
 
 
 def test_close_portal_require_zone_where_is():
