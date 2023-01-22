9c1a151769d96fa96e536068de7de26ed157bb8d
Mathïs Fédérico <mathfederico@gmail.com>
2023-01-22 20:55:25 +0100

    :art: Add custom size for requirements graph

--- src/crafting/examples/minecraft/draw_requirement_graph.py
+++ src/crafting/examples/minecraft/draw_requirement_graph.py
@@ -8,7 +8,8 @@ from crafting.examples.minecraft.world import McWorld
 def draw_minecrafting_requirements_graph():
     """Draw the MineCrafting requirement graph using matplotlib."""
     world = McWorld()
-    fig, ax = plt.subplots(1, 1)
+    fig = plt.figure(0, figsize=(16, 9), dpi=40)
+    ax = fig.add_subplot(111)
     world.draw_requirements_graph(ax)
     return fig, ax
 
