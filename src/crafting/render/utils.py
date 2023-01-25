41ab64adf031a238b9601ebed6a7855f187c2c23
Mathïs Fédérico <mathfederico@gmail.com>
2023-01-25 05:05:28 +0100

    ♻️ Add legacy render

---
+++ src/crafting/render/utils.py
@@ -0,0 +1,214 @@
+# Crafting a meta-environment to simultate inventory managment
+# Copyright (C) 2021-2023 Mathïs FEDERICO <https://www.gnu.org/licenses/>
+
+""" Utilitaries functions for rendering of the Crafting environments """
+
+import os
+from typing import TYPE_CHECKING, Optional, Tuple, Union
+import logging
+
+import numpy as np
+
+from PIL import Image, ImageDraw, ImageFont
+
+
+# pygame is an optional dependency
+try:
+    import pygame
+except ImportError:
+    pass
+
+from crafting.world.items import Item, Tool
+from crafting.world.recipes import Recipe
+from crafting.world.zones import Zone
+
+if TYPE_CHECKING:
+    from pygame.surface import Surface
+
+    from crafting.world.world import World
+
+
+# Disable PIL logging to avoid spam (see https://github.com/camptocamp/pytest-odoo/issues/15)
+logging.getLogger("PIL").setLevel(logging.INFO)
+
+
+def pilImageToSurface(pilImage: Image.Image):
+    """Convert a PIL Image to a pygame Surface.
+
+    Args:
+        pilImage: PIL Image to convert.
+
+    Returns:
+        A pygame Surface.
+
+    """
+    return pygame.image.fromstring(
+        pilImage.tobytes(), pilImage.size, pilImage.mode
+    ).convert_alpha()
+
+
+def create_image(world: "World", obj: Union[Item, Zone, Recipe, str]):
+    """Create a PIL image for and obj in a world.
+
+    Args:
+        world: A crafting World.
+        obj: A crafting Item, Zone, Recipe or property.
+
+    Returns:
+        A PIL image corresponding to the given object.
+
+    """
+
+    def _get_text_color(obj: Union[Item, Zone, Recipe, str]):
+        if isinstance(obj, Item):
+            alt_txt = str(obj.item_id)
+            if isinstance(obj, Tool):
+                color = (0, 255, 255, 255)
+            elif obj in world.foundable_items:
+                color = (0, 125, 0, 255)
+            else:
+                color = (0, 0, 255, 255)
+        elif isinstance(obj, Zone):
+            alt_txt = str(obj)
+            color = None
+        elif isinstance(obj, str):
+            alt_txt = obj
+            color = None
+        elif isinstance(obj, Recipe):
+            return _get_text_color(get_representative_object(obj))
+        else:
+            raise TypeError(f"Unsuported type: {type(obj)} {obj}")
+        return alt_txt, color
+
+    if isinstance(obj, Zone):
+        image_size = (699, 394)
+    else:
+        image_size = (120, 120)
+
+    image = Image.new("RGBA", image_size, (0, 0, 0, 0))
+    draw = ImageDraw.Draw(image)
+
+    cx, cy = image_size[0] // 2, image_size[1] // 2
+    bbox = [0, 0, image_size[0], image_size[1]]
+    alt_txt, color = _get_text_color(obj)
+
+    if color is not None:
+        draw.rectangle(bbox, outline=color, width=5)
+
+    text_pt_size = int(0.60 * image_size[1])
+    font = ImageFont.truetype(world.font_path, size=text_pt_size)
+    draw.text((cx, cy), alt_txt, fill=(0, 0, 0), font=font, anchor="mm")
+    return image
+
+
+def load_image(world: "World", obj: Union[Item, Zone, Recipe, str]):
+    """Load a PIL image for and obj in a world.
+
+    Args:
+        world: A crafting World.
+        obj: A crafting Item, Zone, Recipe or property.
+
+    Returns:
+        A PIL image corresponding to the given object.
+
+    """
+    if obj is None:
+        return None
+
+    if isinstance(obj, Item):
+        image_path = os.path.join(world.resources_path, "items", f"{obj.item_id}.png")
+    elif isinstance(obj, Zone):
+        image_path = os.path.join(world.resources_path, "zones", f"{obj.name}.png")
+    elif isinstance(obj, str):
+        image_path = os.path.join(world.resources_path, "properties", f"{obj}.png")
+    elif isinstance(obj, Recipe):
+        return load_image(world, get_representative_object(obj))
+    else:
+        raise TypeError(f"Unkowned type {type(obj)}")
+
+    return Image.open(image_path).convert("RGBA")
+
+
+def load_or_create_image(
+    world: "World",
+    obj: Union[Item, Zone, Recipe, str],
+    text: Optional[str] = None,
+    text_relative_size: float = 0.3,
+):
+    """Load or create a PIL image for and obj in a world.
+
+    Args:
+        world: A crafting World.
+        obj: A crafting Item, Zone, Recipe or property.
+        text (Optional): Text to draw on top of the image.
+        text_relative_size: If a text is given,
+            this is the relative size of the text compared to the image size.
+
+    Returns:
+        A PIL image corresponding to the given object.
+
+    """
+    try:
+        image = load_image(world, obj)
+    except FileNotFoundError:
+        image = create_image(world, obj)
+
+    if text is not None:
+        image_draw = ImageDraw.Draw(image)
+        image_shape = np.array(image).shape
+
+        text_px_size = int(3 * text_relative_size * min(image_shape[:1]))
+        text_pt_size = int(0.75 * text_px_size)
+        font = ImageFont.truetype(world.font_path, size=text_pt_size)
+        font_offset = (int(0.05 * image_shape[0]), int(0.95 * image_shape[1]))
+        image_draw.text(font_offset, text, font=font, anchor="lb")
+    return image
+
+
+def scale(
+    image: "Surface",
+    canevas_shape: Tuple[int],
+    size_ratio: float,
+    relative_to: str = "width",
+) -> "Surface":
+    """Load and rescales an image using pygame.
+
+    Load and rescales an image relatively to canevas_shape preserving aspect ratio.
+
+    Args:
+        path: Path to the image file.
+        canevas_shape: Shape of the canevas where the image will be blited.
+        size_ratio: Size (in percent) of the loaded image compared to canevas shape.
+        relative_to: One of ('width', 'height'), dimention to consider for size_ratio.
+
+    """
+    image_shape = image.get_size()
+
+    if relative_to.startswith("w"):
+        scale_ratio = int(size_ratio * canevas_shape[0]) / image_shape[0]
+    elif relative_to.startswith("h"):
+        scale_ratio = int(size_ratio * canevas_shape[1]) / image_shape[1]
+    else:
+        raise ValueError(f"Unknowed value for 'relative_to': {relative_to}")
+
+    new_shape = (int(image_shape[0] * scale_ratio), int(image_shape[1] * scale_ratio))
+    return pygame.transform.scale(image, new_shape)
+
+
+def get_representative_object(recipe: Recipe) -> Union[Item, str]:
+    """Get the most representative object for a given recipe.
+
+    Args:
+        recipe (Recipe): The recipe to get the most representative object from.
+
+    Raises:
+        ValueError: The recipe has no output or added_properties, hence no representative object.
+
+    Returns:
+        Union[Item, str]: The most representative object of the given recipe.
+    """
+    if recipe.outputs is not None:
+        return recipe.outputs[0]
+    if len(recipe.added_properties) > 0:
+        return list(recipe.added_properties.keys())[0]
+    raise ValueError(f"Recipe {recipe} has no output nor added_properties.")
---
+++ src/crafting/render/widgets.py
@@ -0,0 +1,203 @@
+# Crafting a meta-environment to simultate inventory managment
+# Copyright (C) 2021-2023 Mathïs FEDERICO <https://www.gnu.org/licenses/>
+
+""" Widgets for rendering of the Crafting environments """
+
+import os
+from typing import TYPE_CHECKING, List, Tuple
+
+import numpy as np
+
+try:
+    from pygame.image import load as load_pygame_image
+    from pygame.font import Font
+except ImportError:
+    pass
+
+from crafting.render.utils import load_or_create_image, pilImageToSurface, scale
+
+if TYPE_CHECKING:
+    from pygame.surface import Surface
+
+    from crafting.env import CraftingEnv
+    from crafting.player.inventory import Inventory
+    from crafting.world.world import World
+    from crafting.world.zones import Zone
+
+
+class EnvWidget:
+
+    """Display widget base class for any crafting environment."""
+
+    def update(self, env: "CraftingEnv"):
+        """Update the widget given the environment state."""
+        raise NotImplementedError
+
+    def draw(self, surface: "Surface"):
+        """Draw the widget on a given surface."""
+        raise NotImplementedError
+
+
+class InventoryWidget(EnvWidget):
+
+    """Displays the player's inventory."""
+
+    def __init__(
+        self,
+        inventory: "Inventory",
+        world: "World",
+        position: Tuple[int],
+        window_shape: Tuple[int],
+    ):
+        self.inventory = inventory
+        self.world = world
+
+        self.background = self._load_background(window_shape)
+        self.shape = self.background.get_size()
+        self.position = np.array(position)
+        self.font = Font(world.font_path, int(0.1 * self.shape[1]))
+
+        self.item_images_per_id = {
+            item_id: self._load_image(item_id) for item_id in self.inventory.items_ids
+        }
+
+    def _load_background(self, window_shape) -> "Surface":
+        background_path = os.path.join(self.world.resources_path, "inventory.png")
+        background_image = load_pygame_image(background_path)
+        return scale(background_image, window_shape, 0.65)
+
+    def _load_image(self, item_id) -> "Surface":
+        image = load_or_create_image(self.world, self.world.item_from_id[item_id])
+        image = pilImageToSurface(image)
+        return scale(image, self.shape, 0.09)
+
+    def update(self, env: "CraftingEnv"):
+        self.inventory = env.player.inventory
+
+    def draw(self, surface: "Surface"):
+        surface.blit(self.background, self.position)
+
+        non_empty_items = self.inventory.content != 0
+
+        items_in_inv = np.array(self.inventory.items)[non_empty_items]
+        content = self.inventory.content[non_empty_items]
+
+        offset = np.array([int(0.029 * self.shape[0]), int(0.155 * self.shape[1])])
+        x_step = y_step = int(0.108 * self.shape[0])
+        font_offset = np.array([int(0.85 * x_step), int(0.9 * y_step)])
+
+        content_position = self.position + offset
+        for i, (item, quantity) in enumerate(zip(items_in_inv, content)):
+            x_offset = (i % 9) * x_step
+            y_last_offset = int(0.22 * y_step) if i // 9 > 2 else 0
+            y_offset = (i // 9) * y_step + y_last_offset
+            item_position = content_position + np.array([x_offset, y_offset])
+            surface.blit(self.item_images_per_id[item.item_id], item_position)
+            if quantity > 1:
+                text_position = item_position + font_offset
+                text = self.font.render(str(quantity), False, "white")
+                text_rect = text.get_rect()
+                text_rect.right = text_position[0]
+                text_rect.bottom = text_position[1]
+                surface.blit(text, text_rect)
+
+
+class ZoneWidget(EnvWidget):
+
+    """Displays the current player zone and its active properties."""
+
+    def __init__(
+        self,
+        zones: List["Zone"],
+        properties: List[str],
+        world: "World",
+        position: Tuple[int],
+        window_shape: Tuple[int],
+    ):
+        self.zone = zones[0]
+        self.position = np.array(position)
+        self.world = world
+
+        self.zones_images = {
+            zone.zone_id: self._load_zone_image(zone.zone_id, window_shape)
+            for zone in zones
+        }
+
+        self.shape = self.zones_images[0].get_size()
+
+        self.properties_images = {
+            prop: self._load_property_image(prop) for prop in properties
+        }
+
+        self.font = Font(world.font_path, int(0.3 * self.shape[1]))
+
+    def _load_zone_image(self, zone_id, window_shape):
+        image = load_or_create_image(self.world, self.world.zone_from_id[zone_id])
+        image = pilImageToSurface(image)
+        return scale(image, window_shape, 0.25)
+
+    def _load_property_image(self, prop: str):
+        image = load_or_create_image(self.world, prop)
+        image = pilImageToSurface(image)
+        return scale(image, self.shape, 0.2)
+
+    def update(self, env: "CraftingEnv"):
+        self.zone = env.player.zone
+
+    def draw(self, surface: "Surface"):
+        zone_image = self.zones_images[self.zone.zone_id]
+        surface.blit(zone_image, self.position)
+
+        zone_name_img = self.font.render(self.zone.name.capitalize(), False, "white")
+        font_shift = np.array([int(0.05 * self.shape[0]), 0])
+        surface.blit(zone_name_img, self.position + font_shift)
+
+        prop_shift = np.array([int(0.02 * self.shape[0]), int(0.90 * self.shape[1])])
+        x_step = int(0.22 * self.shape[0])
+
+        n_active_props = 0
+        for prop, prop_is_true in self.zone.properties.items():
+            if prop_is_true:
+                prop_image = self.properties_images[prop]
+                _, prop_img_height = prop_image.get_size()
+                shift = np.array([n_active_props * x_step, -prop_img_height])
+                prop_position = self.position + prop_shift + shift
+                surface.blit(prop_image, prop_position)
+                n_active_props += 1
+
+
+class ScoreWidget(EnvWidget):
+
+    """Display the current score"""
+
+    def __init__(self, font_path: str, position: Tuple[int], font_size: int):
+        self.position = position
+        self.font = Font(font_path, font_size)
+        self.reward = 0
+        self.score = 0
+
+    def update(self, env: "CraftingEnv"):
+        self.score = env.player.score
+
+    def draw(self, surface: "Surface"):
+        score_name_img = self.font.render(f"SCORE {self.score}", False, "#c95149")
+        surface.blit(score_name_img, self.position)
+
+
+class StepLeftWidget(EnvWidget):
+
+    """Display the number of steps left until the environment is done."""
+
+    def __init__(self, font_path: str, position: Tuple[int], font_size: int):
+        self.position = position
+        self.font = Font(font_path, font_size)
+        self.steps_left = None
+
+    def update(self, env: "CraftingEnv"):
+        self.steps_left = env.max_step - env.steps
+
+    def draw(self, surface: "Surface"):
+        score_name_img = self.font.render(
+            f"Steps left: {self.steps_left}", False, "#803300"
+        )
+        surface.blit(score_name_img, self.position)
