d8b1d94afe9e02779aa457b0a1952691d3a2e56d
Mathïs Fédérico <mathfederico@gmail.com>
2023-01-25 01:18:53 +0100

    :wrench: Refactor CraftingEnv

--- src/crafting/env.py
+++ src/crafting/env.py
@@ -1,6 +1,5 @@
 from typing import List, Set, Tuple, Optional
 
-
 import numpy as np
 
 from crafting.world import Item, ItemStack, Zone, World
@@ -20,11 +19,10 @@ class CraftingEnv:
         self.world = self._build_world()
         self._build_transformations()
 
-        (
-            self.player_inventory,
-            self.position,
-            self.zones_inventories,
-        ) = self._init_state()
+        self.player_inventory = np.array([], dtype=np.uint16)
+        self.position = np.array([], dtype=np.uint16)
+        self.zones_inventories = np.array([], dtype=np.uint16)
+        self._reset_state()
 
     @property
     def state(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
@@ -60,19 +58,14 @@ class CraftingEnv:
         )
         return self._step_output(0)
 
-    def _step_output(self, reward):
+    def _step_output(self, reward: float):
         return (self.observation, reward, self.terminated or self.truncated, {})
 
     def render(self):
         pass
 
     def reset(self, seed: int = 0):
-        # Reset state
-        (
-            self.player_inventory,
-            self.position,
-            self.zones_inventories,
-        ) = self._init_state()
+        self._reset_state()
 
     def _build_world(self) -> World:
         items, zones, zones_items = set(), set(), set()
@@ -94,23 +87,17 @@ class CraftingEnv:
         for transformation in self.transformations:
             transformation.build(self.world)
 
-    def _init_state(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-        player_inventory = np.zeros(self.world.n_items, dtype=np.uint16)
+    def _reset_state(self) -> None:
+        self.player_inventory = np.zeros(self.world.n_items, dtype=np.uint16)
 
-        position = np.zeros(self.world.n_zones, dtype=np.uint16)
+        self.position = np.zeros(self.world.n_zones, dtype=np.uint16)
         start_slot = 0  # Start in first Zone by default
         if self.start_zone is not None:
             start_slot = self.world.slot_from_zone(self.start_zone)
-        position[start_slot] = 1
+        self.position[start_slot] = 1
 
-        zones_inventories = np.zeros(
+        self.zones_inventories = np.zeros(
             (self.world.n_zones, self.world.n_zones_items), dtype=np.uint16
-        )
-
-        return (
-            player_inventory,
-            position,
-            zones_inventories,
         )
 
 
