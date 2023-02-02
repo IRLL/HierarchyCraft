8cec8277d639c8e613265ab9dedb24f5ac78f586
Mathïs Fédérico <mathfederico@gmail.com>
2023-02-02 01:52:12 +0100

    :umbrella: Test test_solve_light_recursive

--- tests/examples/test_recursive.py
+++ tests/examples/test_recursive.py
@@ -74,7 +74,8 @@ def test_light_recursive_requirements_graph():
     n_items = 6
     n_required_previous = 3
     env = LightRecursiveCraftingEnv(
-        n_items=n_items, n_required_previous=n_required_previous
+        n_items=n_items,
+        n_required_previous=n_required_previous,
     )
     expected_graph = nx.DiGraph()
     expected_graph.add_edge(0, 1)
@@ -95,6 +96,35 @@ def test_light_recursive_requirements_graph():
     expected_graph.add_edge(4, 5)
 
     check_isomorphic(env.requirements_graph, expected_graph)
+
+
+def test_solve_light_recursive():
+    n_items = 5  # [0, 1, 2, 3, 4]
+    n_required_previous = 2
+    actions = [
+        0,  # 0
+        1,  # 1 < 0
+        0,  # 0
+        2,  # 2 < 0 + 1
+        0,  # 0
+        1,  # 1
+        3,  # 3 < 1 + 2
+        0,  # 0
+        1,  # 1
+        2,  # 2 < 0 + 1
+        4,  # 4 < 2 + 3
+    ]
+
+    env = LightRecursiveCraftingEnv(
+        n_items=n_items,
+        n_required_previous=n_required_previous,
+    )
+    for action in actions:
+        observation, done, _reward, _info = env.step(action)
+        # Should only see items because no zones
+        check.equal(observation.shape, (n_items,))
+
+    check.is_true(done)
 
 
 # def test_gym_make_lighter_recursive():
