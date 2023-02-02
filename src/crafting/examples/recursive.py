9be8d45bcd39e187ec2d2413b2688fc74baf57fe
Mathïs Fédérico <mathfederico@gmail.com>
2023-02-02 01:56:30 +0100

    :fire: Remove LighterRecursiveCrafting

--- src/crafting/examples/recursive.py
+++ src/crafting/examples/recursive.py
@@ -12,6 +12,7 @@ from typing import List
 from crafting.env import CraftingEnv
 from crafting.world import Item, ItemStack
 from crafting.transformation import Transformation
+from crafting.render.human import render_env_with_human
 
 # gym is an optional dependency
 try:
@@ -131,54 +132,10 @@ class LightRecursiveCraftingEnv(CraftingEnv):
         return transformation
 
 
-# class LighterRecursiveCraftingEnv(CraftingEnv):
-
-#     """LighterRecursive, a lighter version of the LightRecursiveCraftingEnv Environment.
-
-#     Item n requires the k previous items except the item n-2 (n-k to n-3 and n-1).
-
-#     """
-
-#     def __init__(self, n_items: int, n_required_previous: int = 3, **kwargs):
-#         self.n_required_previous = n_required_previous
-#         if self.n_required_previous <= 2:
-#             env_name = "LinearRecursiveCrafting"
-#         else:
-#             env_name = f"LighterRecursiveCrafting-{self.n_required_previous}"
-#         super().__init__(n_items, name=env_name, **kwargs)
-
-#     def _build_recipes(self, items: List[Item]) -> List[Transformation]:
-#         """Build recipes to make every item accessible.
-
-#         Args:
-#             items: List of items.
-
-#         Returns:
-#             List of craft recipes.
-
-#         """
-#         recipes = []
-
-#         for item in items[1:]:
-#             inputs = [ItemStack(items[item.item_id - 1])]
-#             if item.item_id >= 3:
-#                 low_id = max(0, item.item_id - self.n_required_previous)
-#                 inputs += [
-#                     ItemStack(items[item_id])
-#                     for item_id in range(low_id, item.item_id - 2)
-#                 ]
-#             outputs = [ItemStack(items[item.item_id])]
-
-#             # Build recipe
-#             new_recipe = Transformation(inputs=inputs, outputs=outputs)
-#             recipes.append(new_recipe)
-
-#         return recipes
+def main():
+    env = RecursiveCraftingEnv(n_items=7)
+    render_env_with_human(env)
 
 
 if __name__ == "__main__":
-    import gym
-    from crafting.render.human import render_env_with_human
-
-    env = gym.make("RecursiveCrafting-v1", n_items=7, max_step="auto")
-    render_env_with_human(env)
+    main()
--- tests/examples/test_recursive.py
+++ tests/examples/test_recursive.py
@@ -7,11 +7,7 @@ import pytest_check as check
 import networkx as nx
 from networkx import is_isomorphic
 
-from crafting.examples.recursive import (
-    RecursiveCraftingEnv,
-    LightRecursiveCraftingEnv,
-    # LighterRecursiveCraftingEnv,
-)
+from crafting.examples.recursive import RecursiveCraftingEnv, LightRecursiveCraftingEnv
 
 
 def test_gym_make_recursive():
@@ -125,34 +121,6 @@ def test_solve_light_recursive():
         check.equal(observation.shape, (n_items,))
 
     check.is_true(done)
-
-
-# def test_gym_make_lighter_recursive():
-#     gym = pytest.importorskip("gym")
-#     n_items = 10
-#     n_previous_items = 3
-#     env: LighterRecursiveCraftingEnv = gym.make(
-#         "LighterRecursiveCraftingEnv-v1",
-#         n_items=n_items,
-#         n_previous_items=n_previous_items,
-#     )
-#     check.equal(len(env.world.items), n_items)
-#     check.equal(env.name, "LightRecursiveCraftingEnv-K3-I10")
-
-
-# def test_lighter_recursive_requirements_graph(n_items: int, n_required_previous: int):
-#     env = LighterRecursiveCraftingEnv(
-#         n_items=n_items, n_required_previous=n_required_previous
-#     )
-#     expected_graph = DiGraph()
-#     for item in env.world.items:
-#         if item.item_id > 0:
-#             expected_graph.add_edge(item.item_id - 1, item.item_id)
-#         min_idx = max(0, item.item_id - n_required_previous)
-#         for required_id in range(min_idx, item.item_id - 2):
-#             expected_graph.add_edge(required_id, item.item_id)
-
-#     check.is_true(is_isomorphic(env.world.requirements_graph, expected_graph))
 
 
 def check_isomorphic(actual_graph: nx.Graph, expected_graph: nx.Graph):
