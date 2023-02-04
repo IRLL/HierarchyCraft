aa9441d05c6c208c390f8280115aad59455ddc7b
Mathïs Fédérico <mathfederico@gmail.com>
2023-02-04 04:44:52 +0100

    🧪 Update test_graph_unrolled

--- tests/examples/minecraft/test_behaviors.py
+++ tests/examples/minecraft/test_behaviors.py
@@ -7,6 +7,7 @@ import pytest
 import pytest_check as check
 
 import matplotlib.pyplot as plt
+from hebg.unrolling import unroll_graph
 
 from crafting import MineCraftingEnv
 from crafting.examples.minecraft.items import (
@@ -14,6 +15,7 @@ from crafting.examples.minecraft.items import (
     WOOD_PLANK,
     STICK,
     OPEN_NETHER_PORTAL,
+    CRAFTING_TABLE,
 )
 from crafting.examples.minecraft.tools import (
     MC_TOOLS_BY_TYPE_AND_MATERIAL,
@@ -24,7 +26,11 @@ from crafting.examples.minecraft.tools import (
 
 from crafting.examples.minecraft.zones import UNDERGROUND, NETHER
 
-from crafting.behaviors.utils import get_items_in_graph, get_zones_items_in_graph
+from crafting.behaviors.utils import (
+    get_items_in_graph,
+    get_zones_items_in_graph,
+)
+
 
 WOODEN_PICKAXE = MC_TOOLS_BY_TYPE_AND_MATERIAL[ToolType.PICKAXE][Material.WOOD]
 STONE_PICKAXE = MC_TOOLS_BY_TYPE_AND_MATERIAL[ToolType.PICKAXE][Material.STONE]
@@ -50,17 +56,26 @@ class TestItemsInWoodenPickaxeGraph:
             msg=f"\nAll nodes in graph:\n{list(graph.nodes())}",
         )
 
-    # TODO: Allow for unrolling by preventing recursion loops in hebg unrolling
     @pytest.mark.xfail
     def test_graph_unrolled(self):
         """should give all needed items in unrolled wooden_pickaxe graph."""
         expected_items = {WOOD, WOOD_PLANK, STICK, WOODEN_PICKAXE}
-        graph = self.pickaxe_behavior.graph.unrolled_graph
-        items = get_items_in_graph(graph)
+        expected_zone_items = {CRAFTING_TABLE}
+
+        unrolled_graph = unroll_graph(
+            self.pickaxe_behavior.graph, cut_looping_alternatives=True
+        )
+        items = get_items_in_graph(unrolled_graph)
+        zone_items = get_zones_items_in_graph(unrolled_graph)
         check.equal(
             items,
             expected_items,
-            msg=f"\nAll nodes in graph:\n{list(graph.nodes())}",
+            msg=f"\nItems in graph:\n{items}",
+        )
+        check.equal(
+            zone_items,
+            expected_zone_items,
+            msg=f"\nZone items in graph:\n{zone_items}",
         )
 
     def test_graph_draw(self):
