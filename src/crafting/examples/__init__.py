180c6646c9dcf6033617e9341bc7ab78a6a72de9
Mathïs Fédérico <mathfederico@gmail.com>
2023-02-11 16:57:17 +0100

    🎉 Import all examples in crafting.examples

--- src/crafting/examples/__init__.py
+++ src/crafting/examples/__init__.py
@@ -1,3 +1,9 @@
-"""Module for the MineCrafting environement example. """
+"""#Crafting environement examples.
+
+
+"""
 
 from crafting.examples.minecraft.env import MineCraftingEnv
+from crafting.examples.random_simple import RandomCraftingEnv
+from crafting.examples.recursive import RecursiveCraftingEnv, LightRecursiveCraftingEnv
+from crafting.examples.tower import TowerCraftingEnv
