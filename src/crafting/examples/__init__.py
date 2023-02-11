13fc37725229506f88bf6202a6fec3a14820d5e9
Mathïs Fédérico <mathfederico@gmail.com>
2023-02-11 18:51:20 +0100

    📝 Add examples table

--- src/crafting/examples/__init__.py
+++ src/crafting/examples/__init__.py
@@ -1,5 +1,17 @@
 """#Crafting environement examples.
 
+Here is the table of available Crafting environments examples.
+
+If you built one of your own, send us a pull request so we can add it to the list!
+
+| Environment name       | CLI name          |Interest                                              | Reference                               | 
+|:-----------------------|:------------------|:-----------------------------------------------------|:----------------------------------------|
+| MineCrafting           | `minecraft`       | Complex real-case hierarchies.                       |`crafting.examples.minecraft`            |
+| TowerCrafting          | `tower`           | Polynomial growth with reuse hierarchies.            |`crafting.examples.tower`                |
+| RecursiveCrafting      | `recursive`       | Exponential growth with reuse hierarchies.           |`crafting.examples.recursive`            |
+| LightRecursiveCrafting | `light-recursive` | Exponential (smaller) growth with reuse hierarchies. |`crafting.examples.recursive`            |
+| RandomCrafting         | `random`          | Random hierarchical structures.                      |`crafting.examples.random_simple`        |
+
 
 """
 
