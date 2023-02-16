058dd240a9148ee1ee78e6f180f05514dc5e9e24
Mathïs Fédérico <mathfederico@gmail.com>
2023-02-16 17:33:31 +0100

    ✅ Pass test_score_values

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -16,13 +16,14 @@ class TestScore:
         self.purpose.add_task(GetItemTask(Item("wood"), 1), terminal_groups=None)
         self.purpose.add_task(GetItemTask(Item("stone"), 10), terminal_groups="stone")
         self.purpose.add_task(
-            PlaceItemTask(Item("table"), reward=10), terminal_groups="table"
+            PlaceItemTask(Item("table"), reward=15), terminal_groups="table"
         )
         self.env = CraftingEnv(self.world, purpose=self.purpose)
 
     def test_score_values(self):
         actions_per_episodes = []
         actions_per_episodes.append(["search_wood", "craft_plank", "craft_table"])
+        actions_per_episodes.append(["search_wood", "search_stone"])
 
         for episode, actions in enumerate(actions_per_episodes):
             self.env.reset()
@@ -30,8 +31,20 @@ class TestScore:
             for action in actions:
                 transfo = self.named_transformations.get(action)
                 action_id = self.env.world.transformations.index(transfo)
-                _, reward, _, infos = self.env.step(action_id)
+                _, reward, done, infos = self.env.step(action_id)
                 score += reward
+                print(episode, action, reward)
                 if episode == 0:
-                    check.equal(score, infos["score"])
-                    check.equal(score, infos["score_average"])
+                    check.equal(infos["score"], score)
+                    check.equal(infos["score_average"], score)
+                    if done:
+                        check.equal(infos["score"], 16)
+                        check.equal(infos["score_average"], 16)
+                if episode == 1 and done:
+                    check.equal(reward, 10)
+                    check.equal(infos["score"], 1 + 10)
+                    check.equal(
+                        infos["score_average"],
+                        (16 + score) / 2,
+                        msg=f"{self.env.cumulated_score=},{self.env.episodes=}",
+                    )
